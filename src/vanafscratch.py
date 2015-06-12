e799c7852d4e911b4aca3eaad8ed746484373371
Sander Ruben <sanderruben@live.nl>
2015-06-12 15:30:36 +0200

    added movement to the vanafscratch.py script

--- src/vanafscratch.py
+++ src/vanafscratch.py
@@ -27,19 +27,41 @@ from baxter_core_msgs.srv import (
     SolvePositionIKRequest,
 )
 
+def moveto(pose):
+	print("this is the move to function")
+	print(pose)
+
+
 def main():
-#	rospy.init_node('fontys_demo', anonymous=True)
-	rospy.init_node('fontys_demo', anonymous=False)
+	rospy.init_node('fontys_demo', anonymous=True)
 	#Reading Sensors
 	#read joint angles
-	print baxter_interface.Limb('right').joint_angles()
+	print baxter_interface.Limb('left').joint_angles()
 	#read button state
 	print baxter_interface.digital_io.DigitalIO('right_lower_button').state
 	#read IR rangefinder
 	print baxter_interface.analog_io.AnalogIO('right_hand_range').state()
 
-while 0 == 0:
-	print("something")
+	#Enable the actuators
+	baxter_interface.RobotEnable().enable()
+
+	#move to joint positions in radians
+	print("and now for the starting position")
+	baxter_interface.Limb('left').move_to_joint_positions({
+                                                        'left_s0': 0.47, 
+                                                        'left_s1': -0.33, 
+                                                        'left_e0': -1.04, 
+                                                        'left_e1': 1.00, 
+                                                        'left_w0': 0.95, 
+                                                        'left_w1': 1.32, 
+                                                        'left_w2': -1.307 })
+
+
+
+	startpose = [0.6, 0.8, 0.1, math.pi, 0, math.pi]
+	moveto(startpose)
+
+	rospy.spin()
 
 
 
