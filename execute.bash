851931fd510d5c4e6489d87f8ae370603e2288ba
Sander Ruben <sanderruben@live.nl>
2015-06-12 15:25:12 +0200

    added enable robot and prints into the bash script for better user feedback

--- execute.bash
+++ execute.bash
@@ -1,2 +1,6 @@
+echo first enable robot
+rosrun baxter_tools enable_robot.py -e
+echo now tuck the arms
 rosrun baxter_tools tuck_arms.py -u
+echo now it is finally time to start the real demo
 rosrun fontysdemo vanafscratch.py
